a84d14d466c566624984ee7f92d24094140e7fcd
arvin <arvinayaay98@gmail.com>
2020-03-07 20:28:09 -0500

    door to next level spawns after all pillars lit; door is designed to to show a 'portal to the next level

---
+++ scripts/model/door.js
@@ -0,0 +1,11 @@
+function Door(id, width, height, hboxWidth, hboxHeight, xPos, yPos, open) {
+    this.id = id;
+
+    this.width = width;
+    this.height = height;
+    this.hitboxWidth = hboxWidth;
+    this.hitboxHeight = hboxHeight;
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.open = false;
+}

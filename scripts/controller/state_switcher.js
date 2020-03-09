c5a19acc832c1bbf3c5e446cf69af94650d053fb
kelly <kellymi@udel.edu>
2020-03-09 13:51:46 -0400

    toolbar buttons look great now. start game styles added to restart button

--- scripts/controller/state_switcher.js
+++ scripts/controller/state_switcher.js
@@ -1,12 +1,14 @@
 function gameStart(){
     $('#startScreen').css("visibility", "hidden");
     $('#gameBoard').css("visibility", "visible");
+    $('.toolbarButton').css("visibility", "visible");
     gameState.init();
 }
 
 function gameEnd(){
     $('#startScreen').css("visibility", "hidden");
     $('#gameBoard').css("visibility", "hidden");
+    $('.toolbarButton').css("visibility", "hidden");
     $('#deathScreen').css("visibility", "visible");
 }
 
